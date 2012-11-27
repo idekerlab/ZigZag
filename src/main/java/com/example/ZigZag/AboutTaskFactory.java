3476419020bda31310d949d75eb2455b4a456231
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2012-11-27 09:44:07 -0800

    Checking in modified files

--- src/main/java/com/example/ZigZag/AboutTaskFactory.java
+++ src/main/java/com/example/ZigZag/AboutTaskFactory.java
@@ -25,11 +25,16 @@ class AboutTask implements Task
 {
     public void run(TaskMonitor monitor)
     {
+        /*
+         * We have to wrap this in an "invokeLater"
+         * so that the task dialog doesn't show up in the background
+         * of our dialog.
+         */
         SwingUtilities.invokeLater(new Runnable()
         {
             public void run()
             {
-                JOptionPane.showMessageDialog(null, "<html><h1>Zig Zag <small>1.0</small></h1>An incredibly awesome app for finding paths of a node.", "About Zig Zag", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, "<html><h1>Zig Zag <small>1.0</small></h1>An incredibly awesome app for finding paths of a node.</html>", "About Zig Zag", JOptionPane.INFORMATION_MESSAGE);
             }
         });
     }
